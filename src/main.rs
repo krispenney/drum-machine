8df640893858ce88f002925559b292215a4ef3d7
Kris Penney <kristopher.penney@shopify.com>
2019-11-14 22:38:39 -0500

    immutablle iterators

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod sequence;
 
 fn main() {
-  let pattern: [u32; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-  let sequence = sequence::Sequence::new(pattern);
+    let pattern: [u32; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    let sequence = sequence::Sequence::new(pattern);
 
-  println!("{:?}", sequence);
+    println!("{:?}", sequence);
 
-  for v in sequence {
-      println!("{:?}", v);
-  }
+    for v in sequence.into_iter() {
+        println!("{:?}", v);
+    }
+
+    for v in sequence.into_iter() {
+        println!("{:?}", v);
+    }
 }
--- src/sequence.rs
+++ src/sequence.rs
@@ -1,15 +1,15 @@
 #[derive(Debug)]
 pub struct Sequence {
     pattern: [u32; 16],
-    current: usize
 }
 
 impl Sequence {
     pub fn new(pattern: [u32; 16]) -> Sequence {
-        Sequence { pattern: pattern, current: 0 }
+        Sequence { pattern: pattern }
     }
 }
 
+/*
 impl Iterator for Sequence {
     type Item = u32;
 
@@ -22,5 +22,37 @@ impl Iterator for Sequence {
             },
             _ => None,
         }
+    }
+}
+*/
+
+impl<'a> IntoIterator for &'a Sequence {
+    type Item = u32;
+    type IntoIter = SequenceIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        SequenceIterator {
+            sequence: self,
+            index: 0,
+        }
+    }
+}
+
+pub struct SequenceIterator<'a> {
+    sequence: &'a Sequence,
+    index: usize,
+}
+
+impl<'a> Iterator for SequenceIterator<'a> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let result = match self.index {
+            0..=15 => self.sequence.pattern[self.index],
+            _ => return None,
+        };
+
+        self.index += 1;
+        Some(result)
     }
 }
